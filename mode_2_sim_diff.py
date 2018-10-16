ba77c5994af9f6b7baff8816ee9d0de67e3f493e
Bhavyesh Vyas <bhavyas@publicisgroupe.net>
2018-10-16 11:51:23 +0530

    update

--- mode_2_sim_diff.py
+++ mode_2_sim_diff.py
@@ -5,35 +5,56 @@ Created on Tue Oct  9 14:13:16 2018
 @author: bhavyas
 """
 import diff_cli as cli
+import os
+import single_compare
 
-pre_files, post_files = cli.get_folder_paths()
+def mode_2():
 
-if not pre_files == post_files:
-    extra_pre = set(pre_files) - set(post_files)
-    extra_post = set(post_files) - set(pre_files)
+    pre_files, post_files = cli.get_folder_paths()
+    f =[]
+    for i in range(len(pre_files)):
+        if pre_files[i].stem == post_files[i].stem:
+            f.append(True)
+        else:
+            f.append(False)
     
-    pre_files = set(pre_files) - extra_post
-    post_files = set(post_files) - extra_post
-
-
-print("Choose file type\n 1- CSV\n 2- Excel")
-file_type = int(input())
-
-if file_type == 1:    
-    pre, post = cli.read_csv_input(pre_files[0], post_files[0])
-elif file_type == 2:    
-    pre, post = cli.read_xls_input(pre_files[0], post_files[0])    
-
-cols = list(pre.columns)
-    
-primary_key = cli.get_pri_key(cols)
-unimportant_cols = cli.get_drop_cols(cols)
-tolerance = cli.get_tolerance()
-dest_path = cli.get_report_dest()
-
-for file in pre_files:
-    pre_file = file
-    post_file = file
+    if not all(f):
+        extra_pre = set(pre_files) - set(post_files)
+        extra_post = set(post_files) - set(pre_files)
+        
+        pre_files = list(set(pre_files) - extra_pre)
+        post_files =  list(set(post_files) - extra_post)
     
     
+    pre_files.sort()
+    post_files.sort()
+    print("Choose file type\n 1- CSV\n 2- Excel")
+    file_type = int(input())
+    
+    if file_type == 1:    
+        pre, post = cli.read_csv_input(pre_files[0], post_files[0])
+    elif file_type == 2:    
+        pre, post = cli.read_xls_input(pre_files[0], post_files[0])    
+    
+    cols = list(pre.columns)
+        
+    primary_key = cli.get_pri_key(cols)
+    unimportant_cols = cli.get_drop_cols(cols)
+    tolerance = cli.get_tolerance()
+    dest_path = cli.get_report_dest()
+    
+    for i  in range(len(pre_files)):
+        pre_file = pre_files[i]
+        post_file = post_files[i]
+        if file_type == 1:
+            pre, post = cli.read_csv_input(pre_file, post_file)
+        elif file_type == 2:
+            pre, post = cli.read_xls_input(pre_file, post_file)
+        dest_path_i = dest_path + pre_files[i].stem + '\\'
+        os.mkdir(dest_path_i)
+        
+        single_compare.single_compare(pre, post, unimportant_cols, primary_key, tolerance, dest_path_i)
+        
+        
+
     
--- sim_diff_driver.py
+++ sim_diff_driver.py
@@ -5,46 +5,19 @@ Created on Mon Oct  8 14:51:08 2018
 @author: bhavyas
 driver sim diff
 """
-import diff_cli as cli
-import diff_repr
-import diff_summary
+import mode_one_driver as mode_1
+import mode_2_sim_diff as mode_2
+print('Select the SIM_DIFF_MODE:\n 1.Single PRE/POST CSV/Excel File\n 2.PRE/POST Folders\n 3.2GB or greater Single PRE/POST CSV\n 4.Database connect and compare')
 
-print("Choose file type\n 1- CSV\n 2- Excel")
-file_type = int(input())
+mode = int(input())
 
-if file_type == 1:
-    pre_file, post_file = cli.get_file_names()
-    pre, post = cli.read_csv_input(pre_file, post_file)
-elif file_type == 2:
-    pre_file, post_file = cli.get_file_names()
-    pre, post = cli.read_xls_input(pre_file, post_file)
-
-col_diff, col_orphan_pre, col_orphan_post, pre, post = cli.orphan_cols(pre, post)
-
-cols = list(pre.columns)
-
-primary_key = cli.get_pri_key(cols)
-unimportant_cols = cli.get_drop_cols(cols)
-tolerance = cli.get_tolerance()
-dest_path = cli.get_report_dest()
-
-pre, post = cli.hande_unimportant(pre, post, unimportant_cols)
-
-pre = pre.set_index(primary_key)
-post = post.set_index(primary_key)
-
-row_diff, row_orphan_pre, row_orphan_post, pre, post = cli.orphan_rows(pre, post, dest_path)
-
-pre = pre.sort_index()
-post = post.sort_index()
-
-diff_data = []
-
-diff_data = [[col,i,pre[col].iloc[i],post[col].iloc[i]] for col in pre.columns for i in range(len(pre[col])) if pre[col].iloc[i] != post[col].iloc[i]]
-diff_data = cli.drop_tolerance(diff_data, tolerance)
-
-
-
-diff_repr.diff_repr(diff_data, pre.columns, pre, post, dest_path)
-
-diff_summary.get_diff_summary(diff_data, col_diff, col_orphan_pre, col_orphan_post, row_diff, row_orphan_pre, row_orphan_post, dest_path)
+if mode == 1:
+    mode_1.mode_one()
+elif mode == 2:
+    mode_2.mode_2()
+elif mode == 3:
+    print("Under construction")
+elif mode == 4:
+    print("Under construction")
+else:
+    print("Worng Input")
